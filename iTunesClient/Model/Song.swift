933eae1e115334ae2316ed02543d023d12fd1fef
Anna <piladzisi@gmail.com>
2019-04-25 19:31:33 +0300

    song, album model and Stub with actual data to model the behavior of the app

---
+++ iTunesClient/Model/Song.swift
@@ -0,0 +1,18 @@
+//
+//  Song.swift
+//  iTunesClient
+//
+//  Created by anna.sibirtseva on 25/04/2019.
+//  Copyright © 2019 anna.sibirtseva. All rights reserved.
+//
+
+import Foundation
+
+struct Song {
+    let id: Int
+    let name: String
+    let censoredName: String
+    let trackTime: Int
+    let isExplicit: Bool
+}
+
