717083f6e85258043acbcad4dfd07d49bc400c4e
Anna <piladzisi@gmail.com>
2019-04-25 18:58:58 +0300

    model Artist, Genre, Album

---
+++ iTunesClient/Model/Album.swift
@@ -0,0 +1,11 @@
+//
+//  Album.swift
+//  iTunesClient
+//
+//  Created by anna.sibirtseva on 25/04/2019.
+//  Copyright © 2019 anna.sibirtseva. All rights reserved.
+//
+
+import Foundation
+
+class Album {}
