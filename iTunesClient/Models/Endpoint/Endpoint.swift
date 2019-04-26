805ac2a7df8f5c86258850a4bd7a93c8864a7f71
Anna <piladzisi@gmail.com>
2019-04-26 14:10:50 +0300

    model url

--- iTunesClient/Models/Endpoint/Endpoint.swift
+++ iTunesClient/Models/Endpoint/Endpoint.swift
@@ -8,8 +8,54 @@
 
 import Foundation
 
+protocol Endpoint {
+    var base: String { get }
+    var path: String { get }
+    var queryItems: [URLQueryItem] { get }
+}
+extension Endpoint {
+    var urlComponents: URLComponents {
+        var components = URLComponents(string: base)!
+        components.path = path
+        components.queryItems = queryItems
+        
+        return components
+    }
+    
+    var request: URLRequest {
+        let url = urlComponents.url!
+        return URLRequest(url: url)
+    }
+}
+
 enum Itunes {
     case search(term: String, media: ItunesMedia?)
 }
 
 
+extension Itunes: Endpoint {
+    var base: String {
+        return "https://itunes.apple.com"
+    }
+    
+    var path: String {
+        switch self {
+        case .search: return "/search"
+        }
+    }
+    var queryItems: [URLQueryItem] {
+        switch self {
+        case .search(let term, let media):
+            var result = [URLQueryItem]()
+            
+            let searchTermItem = URLQueryItem(name: "term", value: term)
+            result.append(searchTermItem)
+            
+            if let media = media {
+                let mediaItem = URLQueryItem(name: "media", value: media.description)
+                result.append(mediaItem)
+            }
+            return result
+        }
+    }
+}
